354c982491d373fd60639a6e107634ef408d76e7
talhayaseen57 <talhayaseen5725@gmail.com>
2024-09-16 15:45:16 +0200

    added HomeController

--- dotNETControllersApp/Program.cs
+++ dotNETControllersApp/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();      // adds all controller classes in the project as services to the app
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hii! from dotNETControllersApp!");
 
 app.Run();
---
+++ dotNETControllersApp/Properties/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotNETControllersApp.Properties.Controllers
+{
+    public class HomeController
+    {
+        public string method1()
+        {
+            return "Hello! from method1 of HomeController.";
+        }
+    }
+}
