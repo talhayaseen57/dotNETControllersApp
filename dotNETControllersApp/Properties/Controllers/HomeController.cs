54a38a84afae74835d069f3a2532b0c32ff5a2c7
talhayaseen57 <talhayaseen5725@gmail.com>
2024-09-16 17:07:04 +0200

    retuned html type of ContentResult

--- dotNETControllersApp/Properties/Controllers/HomeController.cs
+++ dotNETControllersApp/Properties/Controllers/HomeController.cs
@@ -9,11 +9,16 @@ namespace dotNETControllersApp.Properties.Controllers
         public ContentResult Index()
         {
             //return "Hello! from Index method of HomeController.";     -> for string type return
-            return new ContentResult()
-            {
-                Content = "Hello! from Index method of HomeController.",
-                ContentType = "text/plain"
-            };
+
+            //return new ContentResult()
+            //{
+            //    Content = "Hello! from Index method of HomeController.",
+            //    ContentType = "text/plain"
+            //};
+
+            //return Content("Hello! from Index method of HomeController.", "text/plain");
+
+            return Content("<h1>Welcome</h1><h2>Hi from Index</h2>", "text/html");
         }
 
         // ^??$ -> template for regex expression
