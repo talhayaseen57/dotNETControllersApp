7f9b05252186be3a84350b4cd354b4576b65577e
talhayaseen57 <talhayaseen5725@gmail.com>
2024-09-17 20:50:50 +0200

    added Model Validators

--- dotNETControllersApp/Controllers/AnimalController.cs
+++ dotNETControllersApp/Controllers/AnimalController.cs
@@ -10,15 +10,21 @@ namespace dotNETControllersApp.Controllers
         {
             if (!ModelState.IsValid)
             {
-                List<string> errorsList = new List<string>();
-                foreach (var value in ModelState.Values)
-                {
-                    foreach (var err in value.Errors)
-                    {
-                        errorsList.Add(err.ErrorMessage);
-                    }
-                }
-                string error = string.Join("\n", errorsList);
+                //List<string> errorsList = new List<string>();
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var err in value.Errors)
+                //    {
+                //        errorsList.Add(err.ErrorMessage);
+                //    }
+                //}
+                //string error = string.Join("\n", errorsList);
+
+                //List<string> errorsList = ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList();
+                //string error = string.Join("\n", errorsList);
+
+                string error = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList());
+
                 return BadRequest(error);
             }
 
--- dotNETControllersApp/Models/Animal.cs
+++ dotNETControllersApp/Models/Animal.cs
@@ -4,7 +4,8 @@ namespace dotNETControllersApp.Models
 {
     public class Animal
     {
-        [Required]
+        //[Required]
+        [Required(ErrorMessage = "Animal Name cannot be null or empty.")]
         public string? Name { get; set; }
         public string? ScientificName { get; set; }
         public override string ToString()
