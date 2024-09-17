b93b95b721462442bc2684a6d4f6714a16c3b86f
talhayaseen57 <talhayaseen5725@gmail.com>
2024-09-17 12:46:03 +0200

    added Model Validations

---
+++ dotNETControllersApp/Controllers/AnimalController.cs
@@ -0,0 +1,28 @@
+﻿using dotNETControllersApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNETControllersApp.Controllers
+{
+    public class AnimalController : Controller
+    {
+        [Route("animal")]
+        public IActionResult AnimalMethod(Animal animal)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<string> errorsList = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var err in value.Errors)
+                    {
+                        errorsList.Add(err.ErrorMessage);
+                    }
+                }
+                string error = string.Join("\n", errorsList);
+                return BadRequest(error);
+            }
+
+            return Content(animal.ToString());
+        }
+    }
+}
---
+++ dotNETControllersApp/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNETControllersApp.Models
+{
+    public class Animal
+    {
+        [Required]
+        public string? Name { get; set; }
+        public string? ScientificName { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} - {ScientificName}.";
+        }
+    }
+}
